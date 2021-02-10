deaa02201a1e9853449f6f70636633532b92419e
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-10 19:23:50 -0300

    Added MyFilter

--- filter.js
+++ filter.js
@@ -1,13 +1,35 @@
 const { getPeople } = require('./services');
 
+
+Array.prototype.MyFilter = function (callback){
+    const list = [];
+    for(index in this){
+        const item = this[index];
+        const result = callback(item, index, this);
+
+        if(!result) continue;
+        list.push(item);
+    }
+
+    return list;
+}
+
+
 async function main(){
     try {
         const { results } = await getPeople('a');
 
-        const FamilyLars = results.filter(person => person.name.toLowerCase().indexOf('lars') !== -1);
-        const names = FamilyLars.map(person => person.name);
+        console.time('filter');
+        const FamilyLars1 = results.filter(person => person.name.toLowerCase().indexOf('lars') !== -1);
+        console.timeEnd('filter');
 
-        console.log(names);
+        console.time('MyFilter');
+        const FamilyLars2 = results.MyFilter((person) => person.name.toLowerCase().indexOf('lars') !== -1)
+        console.timeEnd('MyFilter');
+
+        // const names = FamilyLars.map(person => person.name);
+
+        
 
     } catch (error) {
         console.error('Deu Ruim', error);
