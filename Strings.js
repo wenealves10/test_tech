ec765d1f6f0fb5cf0d918d66bd5bbf8ec1fb5d71
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-16 10:28:09 -0300

    More an Challege

---
+++ Strings.js
@@ -0,0 +1,36 @@
+function processData(input) {
+  const dataInput = input.split('\n');
+  const data = dataInput.map((item) => item.trim());
+  const T = parseInt(data[0], 10);
+
+  for(let index = 1; index <=  T; index++){
+    const dataString = [...data[index]];
+    let odd = [];
+    let even = [];
+    for(let indexs in dataString){
+      if(indexs %  2 !== 0){
+        odd.push(dataString[indexs]);
+      }else{
+        even.push(dataString[indexs]);
+      }
+    }
+    console.log(`${even.join('')}  ${odd.join('')}`);
+  }
+
+}
+
+function processData1(input) {
+  let inputCopy = input.split('\n').slice(1);
+  inputCopy.forEach((str) => {
+    let evenStr = '';
+    let oddStr = '';
+      for(let i = 0; i < str.length; i++) {
+          i % 2 ? oddStr += str[i] : evenStr += str[i];
+      }
+      console.log(`${evenStr} ${oddStr}`);
+  });
+}
+
+processData('2\nhacker\nrank')
+
+// console.log(processData('2\nhacker\nrank'))
