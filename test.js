1eb31a166c20356e4678ce7bf64c1c8badbca680
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-16 09:38:12 -0300

    Must return the factors of two matrices

--- test.js
+++ test.js
@@ -254,6 +254,20 @@ function kangaroo(x1, v1, x2, v2) {
   }
 }
 
+function getTotalX(a, b) {
+  let validCount = 0;
+
+  for (let x = 1; x <= 100; x++) {
+      if (a.every(int => (x % int == 0))) {
+          if (b.every(int => (int % x == 0))) {
+
+              validCount++;
+          }
+      }
+  }
+
+  return validCount;
+}
 
 
 describe('HackerRank Challenges', () =>{
@@ -337,6 +351,15 @@ describe('HackerRank Challenges', () =>{
     const expected = 'YES';
 
     const result = kangaroo(2,1,1,2);
+
+    assert.deepStrictEqual(result, expected);
+
+  });
+
+  it('Must return the factors of two matrices', () => {
+    const expected = 2;
+
+    const result = getTotalX([2, 6], [24, 36]);
 
     assert.deepStrictEqual(result, expected);
 
