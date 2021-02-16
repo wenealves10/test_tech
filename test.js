bf5c86c2f8ed1c44ca3aff91a295c2ad9d4bd014
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-16 09:01:46 -0300

    Must return if the two kangaroos arrive at the same

--- test.js
+++ test.js
@@ -242,6 +242,18 @@ function countApplesAndOranges(s, t, a, b, apples, oranges) {
 
 }
 
+function kangaroo(x1, v1, x2, v2) {
+  if ((x1 < x2) && (v1 < v2)){
+    return "NO"
+  }
+  else {
+      if ((v1 != v2) && ((x2 - x1) % (v1 - v2)) == 0)
+          return "YES"
+      else
+          return "NO"
+  }
+}
+
 
 
 describe('HackerRank Challenges', () =>{
@@ -320,5 +332,14 @@ describe('HackerRank Challenges', () =>{
     assert.deepStrictEqual(result, expected);
 
   });
+
+  it('Must return if the two kangaroos arrive at the same', () => {
+    const expected = 'YES';
+
+    const result = kangaroo(2,1,1,2);
+
+    assert.deepStrictEqual(result, expected);
+
+  })
 
 });
