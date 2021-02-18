cb04ff6c74ee00603b1770c98458b87997bcb928
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-18 13:23:19 -0300

    More an challege

---
+++ DictionariesAndMaps.js
@@ -0,0 +1,48 @@
+function processData(input) {
+  const numbersPhones = [];
+  const queryNames = [];
+  let inputCopy = input.split('\n').slice(1)
+
+   inputCopy.forEach(item => {
+    if(item.split(' ').length > 1){
+      numbersPhones.push(item.split(' '));
+    }else{
+      queryNames.push(item);
+    }
+  });
+
+  const phoneBooks = new Map(numbersPhones);
+
+  for(let index in queryNames){
+    if(phoneBooks.has(queryNames[index])){
+      console.log(`${queryNames[index]}=${phoneBooks.get(queryNames[index])}`);
+    }else{
+      console.log("Not found");
+    }
+  }
+
+}
+
+function processData1(input) {
+
+  var tempArray = input.split("\n");
+  var entriesLength = tempArray.splice(0, 1);
+  var queries = tempArray.splice(entriesLength);
+
+  for (var i = 0; i < entriesLength; i++) {
+    tempArray[i] = tempArray[i].split(" ");
+  }
+
+  var phoneBookMap = new Map(tempArray);
+
+  for (var i = 0; i < queries.length; i++) {
+    if (phoneBookMap.has(queries[i])) {
+      console.log(queries[i] + "=" + phoneBookMap.get(queries[i]));
+    } else {
+      console.log("Not found");
+    }
+  }
+}
+
+processData(`3\nsam 99912222\ntom 11122222\nharry 12299933\nsam\nedward\nharry\nwene`)
+processData1(`3\nsam 99912222\ntom 11122222\nharry 12299933\nsam\nedward\nharry`)
