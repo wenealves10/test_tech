b58ec4db3897786a8b8f7cb789af71a9b322b201
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-19 09:38:37 -0300

    More test completed successfully

--- test.js
+++ test.js
@@ -325,6 +325,24 @@ function birthday(s, d, m) {
 
 }
 
+function divisibleSumPairs(n, k, ar) {
+  const lengthArray = n;
+  const numberInteger = k;
+  const arrayInteger = [...ar];
+  let sumPairs = 0;
+
+  for(let i = 0; i < lengthArray; i++){
+    for(let j = 1; j < lengthArray; j++){
+      if(i < j && (arrayInteger[i] + arrayInteger[j]) % numberInteger === 0){
+        sumPairs++;
+      }
+    }
+  }
+
+  return sumPairs
+
+}
+
 
 describe('HackerRank Challenges', () =>{
   it('must return the sum of the matrix diagonals and the difference between them', () =>{
@@ -437,6 +455,15 @@ describe('HackerRank Challenges', () =>{
 
     assert.deepStrictEqual(result, expected);
 
-  })
+  });
+
+  it('The pairs that added and divided must return', () => {
+    const expected = 5;
+
+    const result = divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2]);
+
+    assert.deepStrictEqual(result, expected);
+
+  });
 
 });
