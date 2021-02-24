e5096f260854c30acc968920ec3a06b265f43a96
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-24 06:50:56 -0300

    more challege

---
+++ AbstractClasses.js
@@ -0,0 +1,32 @@
+class Book {
+  constructor(title, author) {
+      if (this.constructor === Book) {
+          throw new TypeError('Do not attempt to directly instantiate an abstract class.');
+      }
+      else {
+          this.title = title;
+          this.author = author;
+      }
+  }
+
+  display() {
+      console.log('Implement the \'display\' method!')
+  }
+}
+
+class MyBook extends Book {
+    constructor(title, author, price){
+        super(title, author);
+        this.price = price;
+    }
+
+    display(){
+        console.log(`Title: ${this.title}`);
+        console.log(`Author: ${this.author}`);
+        console.log(`Price: ${this.price}`);
+    }
+}
+
+const myBook = new MyBook('Livro Java', 'Wene Alves', 14.54);
+
+myBook.display();
